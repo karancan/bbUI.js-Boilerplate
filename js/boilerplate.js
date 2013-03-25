918b671dd241f5ca4c287960e8c1be0b4932f5ad
unknown <karan-91@outlook.com>
2013-03-25 15:06:34 -0400

    Added layout for websql-storage.htm

--- js/boilerplate.js
+++ js/boilerplate.js
@@ -8,7 +8,7 @@
 
 //The following four functions control what happens the user clicks one of the four pill buttons
 		
-function showIntro(){
+function showlocalStorageIntro(){
 	document.getElementById('ls-intro').style.display = 'block';
 	document.getElementById('ls-set').style.display = 'none';
 	document.getElementById('ls-get').style.display = 'none';
@@ -34,4 +34,50 @@ function showDelete(){
 	document.getElementById('ls-set').style.display = 'none';
 	document.getElementById('ls-get').style.display = 'none';
 	document.getElementById('ls-delete').style.display = 'block';
+}
+
+//*******************************
+// storage-websql.htm
+//*******************************
+
+//The following five functions control what happens the user clicks one of the four pill buttons
+
+function showWebSqlIntro(){
+	document.getElementById('ws-intro').style.display = 'block';
+	document.getElementById('ws-create').style.display = 'none';
+	document.getElementById('ws-insert').style.display = 'none';
+	document.getElementById('ws-select').style.display = 'none';
+	document.getElementById('ws-delete').style.display = 'none';
+}
+
+function showCreate(){
+	document.getElementById('ws-intro').style.display = 'none';
+	document.getElementById('ws-create').style.display = 'block';
+	document.getElementById('ws-insert').style.display = 'none';
+	document.getElementById('ws-select').style.display = 'none';
+	document.getElementById('ws-delete').style.display = 'none';
+}
+
+function showInsert(){
+	document.getElementById('ws-intro').style.display = 'none';
+	document.getElementById('ws-create').style.display = 'none';
+	document.getElementById('ws-insert').style.display = 'block';
+	document.getElementById('ws-select').style.display = 'none';
+	document.getElementById('ws-delete').style.display = 'none';
+}
+
+function showSelect(){
+	document.getElementById('ws-intro').style.display = 'none';
+	document.getElementById('ws-create').style.display = 'none';
+	document.getElementById('ws-insert').style.display = 'none';
+	document.getElementById('ws-select').style.display = 'block';
+	document.getElementById('ws-delete').style.display = 'none';
+}
+
+function showRemove(){
+	document.getElementById('ws-intro').style.display = 'none';
+	document.getElementById('ws-create').style.display = 'none';
+	document.getElementById('ws-insert').style.display = 'none';
+	document.getElementById('ws-select').style.display = 'none';
+	document.getElementById('ws-delete').style.display = 'block';
 }
